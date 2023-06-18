73eea73b02d8285567ae9147bf96888a28f1818d
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-06-18 14:56:50 +0000

    [Add] モデル作成

---
+++ app/models/book.rb
@@ -0,0 +1,4 @@
+class Book < ApplicationRecord
+    # アソシエーション
+     belongs_to :user
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
+  get '/home/about' => 'homes#about', as: 'about'
+
   devise_for :users
+
+  resources :books
+
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
