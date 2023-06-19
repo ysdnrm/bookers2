fc7ff49b1ed625cfc7eb4a4612302edfd1b5de4a
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-06-19 14:34:58 +0000

    [Add] 部分テンプレート

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,12 +10,30 @@ class BooksController < ApplicationController
     # current_user は、ログイン中のユーザーの情報を取得できる便利な記述(devise を導入しないと使用することができない)
     @book.user_id = current_user.id
     @book.save
+      flash[:notice] = "You have created book successfully."
     # idが必要だった
       redirect_to book_path(@book.id)
   end
 
   def show
     @book = Book.find(params[:id])
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+     @book = Book.find(params[:id])
+     # 更新内容（book_params）が入っているのが@book
+     # バリデーションの実装は流れが大事
+     if @book.update(book_params)
+       flash[:notice] = "You have updated book successfully."
+       redirect_to book_path(@book.id)
+     else
+      # createとは違い、変数の再定義はいらない
+      render :edit
+     end
   end
 
   def destroy
